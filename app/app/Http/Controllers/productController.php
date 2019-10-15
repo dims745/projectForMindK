a356f6bf2f4dba29f03c6c3abda4f61d78834949
dims745 <dimatimoshen@gmail.com>
2019-10-15 15:32:54 +0300

    finish getting top 10 products

--- app/app/Http/Controllers/productController.php
+++ app/app/Http/Controllers/productController.php
@@ -5,13 +5,42 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Product;
 use App\Category;
+use App\Order;
+use App\OrderItem;
+use Illuminate\Support\Facades\DB;
 
 class productController extends Controller
 {
 
     public function showMostPopularProducts() {
-        $product = Product::all();
-        return response()->json($product);
+        $products = [];
+        $i = 1;
+        $productIds = OrderItem::select('productId', DB::raw('sum(count)'))
+            ->groupBy('productId')
+            ->orderBy(DB::raw(' sum(count) '),'desc')
+            ->take(10)
+            ->get();
+        foreach ($productIds as $productId) {
+            $products[$i] = Product::where('id', $productId->productId)
+                ->first();
+            $i++;
+        }
+        return response()->json($products);
+    }
+
+    public function addOrderItem(Request $req) {
+        $order = new OrderItem();
+        $order->orderId = $req->input('orderId');
+        $order->productId = $req->input('productId');
+        $order->count = $req->input('count');
+        $order->save();
+        $order = OrderItem::all();
+        return response()->json($order);
+    }
+
+    public function showOrderItems() {
+        $orders = OrderItem::all();
+        return response()->json($orders);
     }
 
     public function addCategory(Request $req) {
--- app/routes/api.php
+++ app/routes/api.php
@@ -2,45 +2,18 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-
-
-Route::get('/', function () {
-    return response()->json([
-        'message' => 'nothing'
-    ]);
-});
-
-Route::get('/login', function () {
-    return response()->json([
-        'message' => 'nothing'
-    ]);
-});
-
 Route::post('/login', 'Auth\LoginController@checkLogin');
-
-Route::get('/signIn', function() {
-    return response()->json([
-        'message' => 'nothing'
-    ]);
-});
-
-Route::post('/categories', 'productController@getCategories');
 
 Route::post('/signIn', 'Auth\RegisterController@processingUserData');
 
 Route::post('/verify', 'Auth\LoginController@verifyToken');
 
+Route::post('/categories', 'productController@getCategories');
+
 Route::post('/products/popular', 'productController@showMostPopularProducts');
 
 Route::post('/products/add', 'productController@addProduct');
+
+Route::post('/orderItem/add', 'productController@addOrderItem');
+
+Route::post('/orderItem/show', 'productController@showOrderItems');
